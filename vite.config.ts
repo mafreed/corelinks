0a8277aea019f102ab11f129196415786ee519be
mafreed <49933191+mafreed@users.noreply.github.com>
2025-11-27 16:23:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", // Required for custom domain corelinks.in
+  base: "/", // required for custom domain
 
   plugins: [
-    react(), 
-    mode === "development" && componentTagger()
+    react(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
 
   resolve: {
