ac53d2a26ec9c8476055470e221ecc0830b55341
mafreed <49933191+mafreed@users.noreply.github.com>
2025-11-27 16:18:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
+export default defineConfig({
+  base: '/', // 👈 Must be root when using custom domain
+  plugins: [react()],
+})
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
